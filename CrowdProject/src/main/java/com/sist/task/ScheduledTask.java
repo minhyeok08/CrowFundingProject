80c9bcf1c18b51881bd5401969a6830a850d7f5d
kim <kim@DESKTOP-T2AAIGR>
2023-08-29 17:48:32 +0900

    스케쥴 태스크 수정

--- CrowdProject/src/main/java/com/sist/task/ScheduledTask.java
+++ CrowdProject/src/main/java/com/sist/task/ScheduledTask.java
@@ -14,7 +14,7 @@ public class ScheduledTask {
 
     @Scheduled(cron = "0 0 0 * * ?") // 매일 자정에 실행
     public void updateFundmaking() {
-        String sql = "UPDATE fundmaking SET acno = CASE WHEN openday <= SYSDATE THEN 1 ELSE 3 END";
+        String sql = "UPDATE wadiz_fund_detail SET acno = CASE WHEN openday <= SYSDATE THEN 1 ELSE 3 END";
         jdbcTemplate.update(sql);
         System.out.println("Fundmaking table updated."); // 선택사항: 작업 실행을 확인하기 위한 로그
     }
